38fef89c99a9d1ad2bdc560d3fa05af1507360e4
AugustoTI <augustocesarti2021@gmail.com>
2022-06-20 16:25:41 -0300

    adicionando suporte para navegadores antigos

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,19 @@ import checker from 'vite-plugin-checker';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPath from 'vite-tsconfig-paths';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), tsconfigPath(), checker({ typescript: true })],
+  plugins: [
+    react(),
+    svgr(),
+    tsconfigPath(),
+    checker({ typescript: true }),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
